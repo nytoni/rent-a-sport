7c980c5c10a3a99c1dfcbba3f1cff65f0dd5d859
magicalplatypus <patrickwschwarz@gmail.com>
2016-11-10 14:12:53 -0500

    update cart.java
    I still don't know why I need getters and setters...

--- RentasportProject/src/objectlayer/Cart.java
+++ RentasportProject/src/objectlayer/Cart.java
@@ -1,5 +1,26 @@
 package objectlayer;
 
 public class Cart {
+    bundle[] CartInstance;
+    //each time something is added to cart create new entry in quant with 
+    //number of f bundle purchased
+    int[] quant;
+    double total;
+  
+  
+  
+   public bundle[] getCart(){
+      return cartInstance;
+   }//getCart
+  
+  public double getTotal(){
+      return total;
+  }//getTotal
+  
+  public void setTotal(double newTotal){
+    total = newTotal;
+  }//setTotal
 
-}
+  
+  
+}//Cart
