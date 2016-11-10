ea65df150c10fb278469df808a6677592206de47
magicalplatypus <patrickwschwarz@gmail.com>
2016-11-10 14:22:14 -0500

    update Order.java
    Why I do need these again?

--- RentasportProject/src/objectlayer/Orders.java
+++ RentasportProject/src/objectlayer/Orders.java
@@ -1,5 +1,50 @@
 package objectlayer;
 
 public enum Orders {
-
+        String Addr;
+        //occupy Cc from database
+      String[] cC;
+      String newCC;
+      String newBillAddress;
+      String newCCV;
+      String newName;
+  
+      String CCUsed;
+  
+  
+   public String getAddr(){
+      return getAddr;
+  }//getAddr
+  
+  public void setCreditCard(){
+    //if I decide ites necessary I will add coder here to occupy 
+    //credit cards from database
+  }//setCreditCard
+ 
+  public void SetNewCC(int newCredit){
+    newCC = newCredit;
+  }//setnewCC
+  
+  public void setNewBillAddr(String addr){
+      newBillAddress = addr;
+  }//setNewBillAddr
+  
+  public void setNewCCV(String newC){
+    newCCV = newC;
+  }//setNewCCV
+  
+  public void setNewName(String name){
+    newName= name;
+  }//setnewName
+  
+  public void setCCUsed(int index){
+    CCUsed = cC[index];
+  
+  public int addCCtoData(){
+    if(newCCV==null||newBillAddress==null||newCC==null||newName==null){
+        return 0;
+    }else{
+        //execute database add command in if; return 0 if failed 1 if successful
+        return 1;
+    }//else
 }
