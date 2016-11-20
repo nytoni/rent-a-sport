960ce7f4fa2512f4ea9e2477455fceb322183c92
Drunkorean <mikeis1@uga.edu>
2016-11-19 23:34:29 -0500

    Update UserLogicImpl.java

--- RentasportProject/src/logiclayer/UserLogicImpl.java
+++ RentasportProject/src/logiclayer/UserLogicImpl.java
@@ -1,5 +1,23 @@
 package logiclayer;
 
 public class UserLogicImpl {
-
+  public static HashMap<String, Object> goCreateANewAccount(String, name, String email, String password){
+    
+    String firstName = "";
+    String lastName = "";
+    
+    if(name.contains(" ")){
+      String [] parts = name.split(" ");
+      firstName = parts[0];
+      lastName = parts[1];
+      
+    } else {
+      firstName = name;
+      lastName = "";
+    }
+    
+    int result = Persister.goCreateNewAccountSQLPersistLayer(firstName, lastName, email, password);
+    
+    
+  }
 }
