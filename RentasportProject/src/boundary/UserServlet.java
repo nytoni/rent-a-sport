a9b3bc46372a82625589d7700c78704ab8cc8b95
Drunkorean <mikeis1@uga.edu>
2016-11-19 23:24:39 -0500

    Complete UserServlet

--- RentasportProject/src/boundary/UserServlet.java
+++ RentasportProject/src/boundary/UserServlet.java
@@ -27,7 +27,44 @@ public class UserServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		String name = request.getParameter("name");
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		HashMap<String, Object> data = LogicImplementer.goCreateANewAccount(name, email, password);
+		String path = this.getServletContext().getRealPath("/WEB-INF/template/");
+		
+		Configuration cfg = new Configuration(Configuration.VERSION_2_3_25);
+		cfg.setDirectoryForTemplateLoading(new File(path));
+		cfg.setDefaultEncoding("UTF-8");
+		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+		cfg.setLogTemplateExceptions(false);
+		
+		if(data.get("result") == (Integer) 1){
+			Template template = cfg.getTemplate("successfulRegistration.ftl");
+			Writer out = new OutputStreamWriter(response.getOutputStream());
+			try {
+				template.process(data, out);	
+			} catch (TemplateException e){
+				e.printStackTrace();	
+			}
+			
+		} else {
+			Template template = cfg.getTemplate("un-successfulRegistration.ftl");
+			Writer out = new OutputStreamWriter(response.getOutputStream());
+			try {
+				template.process(data, out);	
+			} catch (TemplateException e){
+				e.printStackTrace();	
+			}
+		
+		}
+			
+			
+		}
+	
 	}
 
 	/**
