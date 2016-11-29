35f43a9593d33c605c3b6775c6037ee0f1b8c129
Drunkorean <mikeis1@uga.edu>
2016-11-29 14:24:01 -0500

    Update UserLogicImpl.java

--- RentasportProject/src/logiclayer/UserLogicImpl.java
+++ RentasportProject/src/logiclayer/UserLogicImpl.java
@@ -34,6 +34,24 @@ public class UserLogicImpl {
   }
   public static HashMap<String, Object> logIn(String email, String password){
       ResultSet rset = UserPersister.goGetLoginInfo(email, password);
-    
+      HashMap<String, Object> data = new HashMap<>();
+      try {
+        if(!rset.next()){
+          data.put("result", 0);
+          data.put("email", email);
+        }
+        else{
+          data.put("result", 1);
+          data.put("customerId", rset.getString(1));
+          data.put("name", rset.getString(2));
+          data.put("password", rset.getString(3));
+          data.put("email", rset.getString(4));
+          
+        }
+      } catch (SQLException e) {
+        e.printStackTrace(); 
+      }
+      
+      return data;
   }
 }
