735cfa7083b8fd0135f97a40e3f4ff7793a0281a
Drunkorean <mikeis1@uga.edu>
2016-11-29 14:38:44 -0500

    Update UserLogicImpl.java

--- RentasportProject/src/logiclayer/UserLogicImpl.java
+++ RentasportProject/src/logiclayer/UserLogicImpl.java
@@ -6,27 +6,16 @@ import objectlayer.*;
 public class UserLogicImpl {
   public static HashMap<String, Object> goCreateANewAccount(String name, String email, String password){
     
-    String firstName = "";
-    String lastName = "";
+   
     
-    if(name.contains(" ")){
-      String [] parts = name.split(" ");
-      firstName = parts[0];
-      lastName = parts[1];
-      
-    } else {
-      firstName = name;
-      lastName = "";
-    }
-    
-    int result = UserPersister.goCreateNewAccountSQLPersistLayer(firstName, lastName, email, password);
+    int result = UserPersister.goCreateNewAccountSQLPersistLayer(name, email, password);
     HashMap<String, Object> data = new HashMap<>();
     
     if(result == 1) {
       data.put("result", 1); 
     } else {
       int userId = UserPersister.getUserId(email);
-      User currentUser = new User(userId, firstName, lastName, email, password);
+      User currentUser = new User(userId, name, email, password);
       data.put("currentUser", currentUser);
     }
     return data;
