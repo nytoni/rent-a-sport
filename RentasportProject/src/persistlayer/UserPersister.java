caf6c4d306a3f4eeeea416f7f4bbe929c894a00d
Julian Jordan <julianjordn@gmail.com>
2016-12-04 22:58:12 -0500

    Update UserPersister.java

--- RentasportProject/src/persistlayer/UserPersister.java
+++ RentasportProject/src/persistlayer/UserPersister.java
@@ -1,13 +1,16 @@
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+
+//import java.sql.Connection;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
 
 public class UserPersister {
   static Connection con = DbAccessImpl.connect();
  
   public static int goCreateNewAccountSQLPersistLayer(String name, String email, String password){
     
-    String addRequest = "insert into User (Name, Email, Password) values ('" +  name + "','" + email + "','" + password + "');";
+    String addRequest = "insert into UserNames (Name, Email, Password) values ('" +  name + "','" + email + "','" + password + "');";
     return DbAccessImpl.createAccount(con, addRequest);
   
   }//newAccount
@@ -18,38 +21,50 @@ public class UserPersister {
   
  }//getUserId
  
- public static ResultSet getCheckout(userId){
+ public static ResultSet getCheckout(int userId){
   String idQuery ="select cardNum from account where customerId ='"+userId+"';";
   return DbAccessImpl.retrieve(con,idQuery);
   
  }//getCheckout
 
- public static resultSet getReviewOrder(userId){
+ public static ResultSet getReviewOrder(int userId){
   String idQuery = "select * from orders where customerId ='"+userId+
    "' UNION select bundleName, quantity from cart where customerId = '"+userId+"';";
    return DbAccessImpl.retrieve(con,idQuery);
   
 }//getReviewOrder
 
- public statuc resultSet getMyCart(userId){
+ public static ResultSet getMyCart(int userId){
   String idQuery = "select bundleName, quantity, price from cart where customerId='"+userId+"';";
   return DbAccessImpl.retrieve(con,idQuery);
  }//getMyCart
 
   public static void changePassword(int userId, String confirmPassword){
-    String changeRequest = "update user set password = '" + confirmPassword + "', where customerId = '"+userId + "';";
+    String changeRequest = "update user set password = '" + confirmPassword + "' where customerId = '"+userId + "';";
+    System.out.println(changeRequest);
     int whatever = DbAccessImpl.changePassword(con, changeRequest);
   }  
   
   public static ResultSet checkOldPassword(int userId){
     String oldPasswordQuery = "select password from user where customerId = '" + userId + "';"; 
-    return DbAcessImpl.retrieve(con, oldPasswordQuery);
+    return DbAccessImpl.retrieve(con, oldPasswordQuery);
   }
   
   public static ResultSet goGetLoginInfo(String email, String password){
     String getRequest = "select * from user where email = '" + email + "' AND password = '" + password + "';";
-    return DatabaseAccess.retrieve(con, getRequest);
+    return DbAccessImpl.retrieve(con, getRequest);
     
   }
-                                   
+
+  public static void changeCreditCardInfo(int customerId,String name, String billingAddress, String shippingAddress,String cardNumber, String expireDate, String securityCode){
+	  String changeRequest= "update account set creditName = '" + name+"',shippingAddr = '" + shippingAddress+"', billingAddr = '" + billingAddress+"' , cardNum = '" + cardNumber+"',expirationDate = '" + expireDate+"',securityCode = '" + securityCode+"' where customerId  = '" + customerId+"'";
+	  int whatever = DbAccessImpl.changePassword(con, changeRequest);
+  }
+  
+  
+  public static ResultSet getCartInfo(int userId){
+	  String query = "select * from orders where customerId = '" + userId + "';";
+	  return DbAccessImpl.retrieve(con, query);
+  }
+  
 }
