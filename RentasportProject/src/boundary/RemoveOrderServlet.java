8c4167ff394cd5f3a0bdd4f249e9f4517690853a
Julian Jordan <julianjordn@gmail.com>
2016-12-05 22:33:10 -0500

    Create RemoveOrderServlet.java

---
+++ RentasportProject/src/boundary/RemoveOrderServlet.java
@@ -0,0 +1,94 @@
+
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
+/**
+ * Servlet implementation class RemoveOrderServlet
+ */
+@WebServlet("/RemoveOrderServlet")
+public class RemoveOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RemoveOrderServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String userIdString = request.getParameter("userId");
+		String orderIdString = request.getParameter("radio");
+//		System.out.println(userIdString);
+		int userId = Integer.parseInt(userIdString);
+		int orderId = Integer.parseInt(orderIdString);
+		HashMap<String, Object> data = ProduceCartLogicImpl.removeOrder(userId,orderId);
+
+		String path = this.getServletContext().getRealPath("/WEB-INF/template/");		
+		Configuration cfg = new Configuration(Configuration.VERSION_2_3_25);
+		cfg.setDirectoryForTemplateLoading(new File(path));	
+		cfg.setDefaultEncoding("UTF-8");
+		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+
+		cfg.setLogTemplateExceptions(false);
+		if(data.get("thereIsItem")==(Integer)1){
+			Template template = cfg.getTemplate("reviewOrder.ftl");
+			Writer out = new OutputStreamWriter(response.getOutputStream());
+			try {
+				template.process(data,out);
+			} catch (TemplateException e) {
+				e.printStackTrace();	
+			}
+		}
+		else{
+			Template template = cfg.getTemplate("noOrder.ftl");
+			Writer out = new OutputStreamWriter(response.getOutputStream());
+			try {
+				template.process(data,out);
+			} catch (TemplateException e) {
+				e.printStackTrace();	
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	
+	
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
