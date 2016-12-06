2088e39ef6cb4307608315b77d23f5fe6b4e0354
Julian Jordan <julianjordn@gmail.com>
2016-12-05 22:38:59 -0500

    Update EditAccountLogicImpl.java

--- RentasportProject/src/logiclayer/EditAccountLogicImpl.java
+++ RentasportProject/src/logiclayer/EditAccountLogicImpl.java
@@ -1,36 +1,56 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 
-package logiclayer;
+//package logiclayer;
 
-import persistlayer.*;
-import objectlayer.*;
+//import persistlayer.*;
+//import objectlayer.*;
 
 
 public class EditAccountLogicImpl {
   public static HashMap<String, Object> changePassword(int userId, String confirmPassword){
     
     //go down ladder and change to void
+	 System.out.println(confirmPassword);
     UserPersister.changePassword(userId, confirmPassword);
     HashMap<String, Object> data = new HashMap<>();
-   
     data.put("userId", userId);
+ 
     return data;
     
     
   }
   
-  public static int checkOldPassword(int userId, String oldPassId){
-    ResultSet result = UserPersister.checkOldPassword(userId);
-    
-    if(result.getString(1) == oldPassId){
-      return 1; 
-    } else {
-      return 0;
-    }
-    
-    
+public static HashMap<String, Object> changeCreditCardInfo(int customerId,String name, String billingAddress, String shippingAddress,String cardNumber, String expireDate, String securityCode){
+
+	    UserPersister.changeCreditCardInfo(customerId,name, billingAddress,shippingAddress,cardNumber,expireDate,securityCode);
+	    HashMap<String, Object> data = new HashMap<>();
+	    data.put("userId", customerId);
+	  
+	    return data;
   }
   
-  
+  public static int checkOldPassword(int userId, String oldPassId) throws SQLException{
+	  
+	 
+    ResultSet result = UserPersister.checkOldPassword(userId);
+
+    String password = "";
+    while(result.next())
+    {
+    	password = result.getString("password");
+    }
+
+    if(password.equals(oldPassId)){
+    	System.out.println("1");
+      return 1; 
+    } 
+    else {//nothing prints out in else for some reason
+    	System.out.print("0");
+      return 0;
+    }
+  }
   
   
 }
