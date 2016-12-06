e138106658a90acf4bb6cea6a82cd5b800d49846
magicalplatypus <patrickwschwarz@gmail.com>
2016-12-06 08:01:14 -0500

    comments

--- RentasportProject/src/persistlayer/UserPersister.java
+++ RentasportProject/src/persistlayer/UserPersister.java
@@ -27,6 +27,7 @@ public class UserPersister {
   
  }//getCheckout
 
+	//query orders table for bundleName quantity from cart where specific customer id
  public static ResultSet getReviewOrder(int userId){
   String idQuery = "select * from orders where customerId ='"+userId+
    "' UNION select bundleName, quantity from cart where customerId = '"+userId+"';";
@@ -34,11 +35,13 @@ public class UserPersister {
   
 }//getReviewOrder
 
+	//return bundlename quantity and price from cart for a specific userId
  public static ResultSet getMyCart(int userId){
   String idQuery = "select bundleName, quantity, price from cart where customerId='"+userId+"';";
   return DbAccessImpl.retrieve(con,idQuery);
  }//getMyCart
 
+	
   public static void changePassword(int userId, String confirmPassword){
     String changeRequest = "update user set password = '" + confirmPassword + "' where customerId = '"+userId + "';";
     System.out.println(changeRequest);
@@ -49,32 +52,38 @@ public class UserPersister {
     String oldPasswordQuery = "select password from user where customerId = '" + userId + "';"; 
     return DbAccessImpl.retrieve(con, oldPasswordQuery);
   }
+	//return cardnum and shipping addr from account for specific customerid
 	public static ResultSet getCheckout(String userId){
   String idQuery ="select cardNum,shippingAddr from account where customerId ='"+userId+"';";
   return DbAccessImpl.retrieve(con,idQuery);
  }//getCheckout
 
+	//return time bundlename and quantity from cart where certain custerid and ordernum is null
  public static ResultSet getCart(String time, int quantity, String bundleName, int userId){
 	 String idQuery = "select"+time+","+bundleName+","+ quantity +"from cart where customerId ='"+userId+"'AND orderNum IS NULL;";
 	 return DbAccessImpl.retrieve(con, idQuery);
  }//getcart
   
+	//return quantity, bundleName and time from cart where specific customerid and ordernum is null
  public static ResultSet getCartInfo(int userId){
 	 String idQuery ="select quantity, bundleName, time from cart where customerId ='"+userId+"'AND orderNum IS NULL;";
 	 return DbAccessImpl.retrieve(con, idQuery);
  }//getCartInfo
  
+	return shipping address, billing address, cardnumber and expiration date from account with certain userId
  public static ResultSet getOrderPageInfo(int userId){
 	 String idQuery ="select shippingAddr, billingAddr, cardNum, expirationDate from account where customerId='"+userId+"';";
 	 return DbAccessImpl.retrieve(con, idQuery);
  }//getOrderPage
  
+	//populate all the fields of order table with pre formatted string
  public static int addOrderToDatabase(String query){
 	 //add order to database
 	 DbAccessImpl db = new DbAccessImpl();
 	 return db.create(con, query);	 
  }
  
+	//figure out the order number of the recently added order
  public static int findOutOrderNum(String query){
 	 	ResultSet rs = DbAccessImpl.retrieve(con,query);
 	 	try{
@@ -86,14 +95,16 @@ public class UserPersister {
 	 // return 1;
  }//findOutOrderNum
  
+	//take the newly found order num and add it the cart entries that do not have an order num and are under certain name
  public static int addOrderNumToCart(String query){
 	 DbAccessImpl db = new DbAccessImpl();
 	 return db.create(con, query);	
  }//addOrderNumToCart
  
- 
+ 	//remove a certain bundle name from the cart
  public static void removeBundleFromCart(String bundleName, int userId){
-		String removeCart = "DELETE * from cart where bundleName ='"+bundleName+"' AND userId = '"+userId+"';";
+		//String removeCart = "DELETE * from cart where bundleName ='"+bundleName+"' AND userId = '"+userId+"';";
+	 	String removeCart = "DELETE from cart where bundleName ='"+bundleName+"' AND userId = '"+userId+"';";
 		DbAccessImpl db = new DbAccessImpl();
 		db.delete(con,removeCart);
 	}//removeBundleFromCart
