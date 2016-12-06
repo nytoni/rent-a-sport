71be4800ba70c8ea77bb7465df7cb50b03bdb2d7
Julian Jordan <julianjordn@gmail.com>
2016-12-05 22:40:38 -0500

    Update EditAccount.java

--- RentasportProject/src/boundary/EditAccount.java
+++ RentasportProject/src/boundary/EditAccount.java
@@ -1,12 +1,20 @@
+///package boundary;
 
-package boundary;
+import java.io.*;
+import java.io.File;
+import java.sql.SQLException;
+import java.util.HashMap;
 
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
 
 /**
  * Servlet implementation class CartServlet
@@ -29,27 +37,45 @@ public class EditAccount extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		String oldPassId = request.getParamter("oldPassId");
+		String oldPassId = request.getParameter("oldPassId");
 		String newPassId = request.getParameter("newPassId");
-		String confirmPass = request.getParameter("confirmPass");
-		int userId = request.getParameter("userId");
-		
-		int didTheVerificationGetThrough = EditAccountLogicImpl.checkOldPassword(userId, oldPassId);
-		
+		String confirmPass = request.getParameter("confirmPassId");
+		String userIdString = request.getParameter("userId");
+		int userId = Integer.parseInt(userIdString);
+		boolean dTVGTb =true;
+		int didTheVerificationGetThrough = 0;
+		try {
+			didTheVerificationGetThrough = EditAccountLogicImpl.checkOldPassword(userId, oldPassId);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		if(didTheVerificationGetThrough ==1)
+		{
+			dTVGTb =true;
+		}
+		else{
+			dTVGTb = false;
+
+			}
 		String path = this.getServletContext().getRealPath("/WEB-INF/template/");
 		Configuration cfg = new Configuration(Configuration.VERSION_2_3_25);
 		cfg.setDirectoryForTemplateLoading(new File(path));
+	
 		cfg.setDefaultEncoding("UTF-8");
+
 		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+	
 		cfg.setLogTemplateExceptions(false);
+
 		
-		
-		if(didTheVerificationGetThrough){
+		if(dTVGTb){
 			HashMap<String, Object> data = EditAccountLogicImpl.changePassword(userId, confirmPass);
 			Template template = cfg.getTemplate("successfulPasswordChange.ftl");
 			Writer out = new OutputStreamWriter(response.getOutputStream());
 			try {
-				template.process(data.out);
+				template.process(data,out);
 			} catch (TemplateException e) {
 				e.printStackTrace();	
 			}
@@ -59,7 +85,7 @@ public class EditAccount extends HttpServlet {
 			Template template = cfg.getTemplate("un-successfulPasswordChange.ftl");
 			Writer out = new OutputStreamWriter(response.getOutputStream());
 			try {
-				template.process(data.out);
+				template.process(data,out);
 			} catch (TemplateException e) {
 				e.printStackTrace();	
 			}
@@ -67,7 +93,7 @@ public class EditAccount extends HttpServlet {
 		}
 		
 		
-		
+
 		
 	}
 
