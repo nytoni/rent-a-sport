1922bbe9d3573f2461eb0be387a6d97dd55a4b4b
magicalplatypus <patrickwschwarz@gmail.com>
2016-12-06 02:52:08 -0500

    user persister updates with all of my changes

--- RentasportProject/src/persistlayer/UserPersister.java
+++ RentasportProject/src/persistlayer/UserPersister.java
@@ -49,6 +49,55 @@ public class UserPersister {
     String oldPasswordQuery = "select password from user where customerId = '" + userId + "';"; 
     return DbAccessImpl.retrieve(con, oldPasswordQuery);
   }
+	public static ResultSet getCheckout(String userId){
+  String idQuery ="select cardNum,shippingAddr from account where customerId ='"+userId+"';";
+  return DbAccessImpl.retrieve(con,idQuery);
+ }//getCheckout
+
+ public static ResultSet getCart(String time, int quantity, String bundleName, int userId){
+	 String idQuery = "select"+time+","+bundleName+","+ quantity +"from cart where customerId ='"+userId+"'AND orderNum IS NULL;";
+	 return DbAccessImpl.retrieve(con, idQuery);
+ }//getcart
+  
+ public static ResultSet getCartInfo(int userId){
+	 String idQuery ="select quantity, bundleName, time from cart where customerId ='"+userId+"'AND orderNum IS NULL;";
+	 return DbAccessImpl.retrieve(con, idQuery);
+ }//getCartInfo
+ 
+ public static ResultSet getOrderPageInfo(int userId){
+	 String idQuery ="select shippingAddr, billingAddr, cardNum, expirationDate from account where customerId='"+userId+"';";
+	 return DbAccessImpl.retrieve(con, idQuery);
+ }//getOrderPage
+ 
+ public static int addOrderToDatabase(String query){
+	 //add order to database
+	 DbAccessImpl db = new DbAccessImpl();
+	 return db.create(con, query);	 
+ }
+ 
+ public static int findOutOrderNum(String query){
+	 	ResultSet rs = DbAccessImpl.retrieve(con,query);
+	 	try{
+	 		return rs.getInt(1);
+	 	}catch(SQLException e){
+	 		System.out.println("something went wrong");
+	 		return -1;
+	 	}
+	 // return 1;
+ }//findOutOrderNum
+ 
+ public static int addOrderNumToCart(String query){
+	 DbAccessImpl db = new DbAccessImpl();
+	 return db.create(con, query);	
+ }//addOrderNumToCart
+ 
+ 
+ public static void removeBundleFromCart(String bundleName, int userId){
+		String removeCart = "DELETE * from cart where bundleName ='"+bundleName+"' AND userId = '"+userId+"';";
+		DbAccessImpl db = new DbAccessImpl();
+		db.delete(con,removeCart);
+	}//removeBundleFromCart
+
   
   public static ResultSet goGetLoginInfo(String email, String password){
     String getRequest = "select * from user where email = '" + email + "' AND password = '" + password + "';";
